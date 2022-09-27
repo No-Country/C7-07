ea47a1a5281190a0e207b769fafb203c868d353b
IgnFed <ignaciofedorenco2317@gmail.com>
2022-09-27 14:02:14 -0300

    fix: changed 'main' field on package.json, adapted for monorepo

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,11 +1,11 @@
-import { StatusBar } from 'expo-status-bar';
-import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { StatusBar } from "expo-status-bar";
+import { SafeAreaProvider } from "react-native-safe-area-context";
 
-import useCachedResources from './hooks/useCachedResources';
-import useColorScheme from './hooks/useColorScheme';
-import Navigation from './navigation';
+import useCachedResources from "./hooks/useCachedResources";
+import useColorScheme from "./hooks/useColorScheme";
+import Navigation from "./navigation";
 
-export default function App() {
+function App() {
   const isLoadingComplete = useCachedResources();
   const colorScheme = useColorScheme();
 
@@ -20,3 +20,5 @@ export default function App() {
     );
   }
 }
+
+export default App;
