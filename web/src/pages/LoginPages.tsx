bb57736dc6cfbc2fefe48c8bfe5174aaba464a7b
MendietaSergio <mendieta.sergio.1995@gmail.com>
2022-09-28 22:59:43 -0300

    add: dependency, pages, components

---
+++ web/src/pages/LoginPages.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Login } from '../components/Login'
+
+export const LoginPage = () => {
+    return (
+        <>
+            <h1>
+                Login pages
+            </h1>
+            <Login />
+        </>
+    )
+}
---
+++ web/src/pages/RegisterPages.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Register } from '../components/Register'
+
+export const RegisterPage = () => {
+  return (
+    <>
+      <h1>
+        Register pages
+      </h1>
+      <Register/>
+    </>
+  )
+}
