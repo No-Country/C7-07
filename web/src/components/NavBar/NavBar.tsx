42f330449d83495a7d64c9f971eeb63bda582b3b
IgnFed <ignaciofedorenco2317@gmail.com>
2022-10-10 17:57:46 -0300

    merge: saved files merged

--- web/src/components/NavBar/NavBar.tsx
+++ web/src/components/NavBar/NavBar.tsx
@@ -1,6 +1,5 @@
 import {
   Box,
-<<<<<<< HEAD
   Image,
   ImageProps,
   List,
@@ -115,106 +114,6 @@ function NavBar() {
         </ListItem>
       </List>
       <Outlet />
-=======
-  Flex,
-  HStack,
-  Image,
-  Text,
-  useBreakpointValue,
-} from "@chakra-ui/react";
-import casaDesactivada from "../../assets/img/casa_desactivado.png";
-import casaActivada from "../../assets/img/casa_activado.png";
-import montanaActivada from "../../assets/img/montana_activado.png";
-import montanaDesactivada from "../../assets/img/montana_desactivado.png";
-import menu from "../../assets/img/menu.png";
-import logoMain from "../../assets/img/Logo_Travis.svg";
-import { Dispatch, useState } from "react";
-import MenuNavBar from "../MenuNavBar/MenuNavBar";
-import { Link } from "react-router-dom";
-/* interface Props {
-  setTogglePosts: Dispatch<boolean>;
-} */
-// function NavBar({ setTogglePosts }: Props) {
-function NavBar() {
-  const [casaIcon, setCasaIcon] = useState(false);
-  const [tourIcon, setTourIcon] = useState(true);
-
-  const burguer = useBreakpointValue({
-    base: "block",
-    md: "none",
-  });
-
-  return (
-    <Box
-      h="80px"
-      display="flex"
-      justifyContent="space-between"
-      alignItems="center"
-      borderBottom="1px solid
-      gray"
-    >
-      <Box
-        display="flex"
-        alignItems={"center"}
-        h="40px"
-        paddingLeft={[2, 3, 10]}
-      >
-        <Image src={logoMain}></Image>
-      </Box>
-
-      <Box
-        display="flex"
-        alignItems={"center"}
-        justifyContent={["center", "center", "center"]}
-        flex={1}
-      >
-        <Link to={"/"}>
-          <Box
-            marginRight={[2, 5, 5]}
-            onClick={() => {
-              // setTogglePosts(false);
-              if (casaIcon) {
-                setCasaIcon(false);
-                setTourIcon(true);
-              }
-            }}
-          >
-            {casaIcon ? (
-              <Image src={casaDesactivada} w="40px" />
-            ) : (
-              <Image src={casaActivada} w="40px" />
-            )}
-          </Box>
-        </Link>
-        <Link to={"tours"}>
-          <Box
-            marginLeft={[2, 5, 5]}
-            onClick={() => {
-              // setTogglePosts(true);
-              if (tourIcon) {
-                setTourIcon(false);
-                setCasaIcon(true);
-              }
-            }}
-          >
-            {tourIcon ? (
-              <Image src={montanaDesactivada} w="50px" paddingTop={3} />
-            ) : (
-              <Image src={montanaActivada} w="50px" paddingTop={3} />
-            )}
-          </Box>
-        </Link>
-      </Box>
-
-      <Box display="flex" alignItems={"center"} paddingRight={[2, 3, 10]}>
-        <Box>
-          <Image src={menu} display={burguer} w="40px" />
-        </Box>
-        <Box display={["none", "none", "block"]} maxW={"160px"} zIndex={10}>
-          <MenuNavBar />
-        </Box>
-      </Box>
->>>>>>> 83ce6775aaea0fd929396a502909ba7c761d784f
     </Box>
   );
 }
--- web/src/main.tsx
+++ web/src/main.tsx
@@ -3,20 +3,13 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 import { store } from "./app/store";
-<<<<<<< HEAD
 import { theme } from "./themes";
-=======
->>>>>>> 83ce6775aaea0fd929396a502909ba7c761d784f
 import { Provider } from "react-redux";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <>
     <Provider store={store}>
-<<<<<<< HEAD
       <ChakraProvider theme={theme}>
-=======
-      <ChakraProvider>
->>>>>>> 83ce6775aaea0fd929396a502909ba7c761d784f
         <App />
       </ChakraProvider>
     </Provider>
