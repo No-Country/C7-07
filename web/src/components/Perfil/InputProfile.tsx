043026aec1a99a7e93802a11cd11f4f57ce61fac
IgnFed <ignaciofedorenco2317@gmail.com>
2022-10-10 19:23:52 -0300

    fix: responsive

--- web/src/components/Perfil/InputProfile.tsx
+++ web/src/components/Perfil/InputProfile.tsx
@@ -5,28 +5,23 @@ import candado from "../../assets/img/candado.svg";
 import { wrap } from "popmotion";
 import { Link } from "react-router-dom";
 
-function Contraseña() {
+function InputProfile() {
   return (
-    <Box
-      display={"flex"}
-      paddingX={[4, 4, 10]}
-      flexDirection="column"
-      w={["full"]}
-    >
-      <Box marginTop={["15px", "20px"]}>
+    <>
+      <Box>
         <Text
           fontWeight="bold"
           fontSize={["x-large", "x-large", "xx-large"]}
           borderBottom="1px solid #B5B5B5"
         >
-          Cambiar contraseña
+          Detalles del perfil
         </Text>
       </Box>
 
-      <Box display={"flex"} paddingBottom="25px" flexWrap="wrap">
+      <Box display={"flex"} flexDirection={"row"} flexWrap="wrap">
         <Input
           variant="filled"
-          placeholder="Nueva contraseña"
+          placeholder="Nombres"
           marginY={[3, 3, 5]}
           marginX={[4, 4, 5]}
           minW="300px"
@@ -37,7 +32,42 @@ function Contraseña() {
         />
         <Input
           variant="filled"
-          placeholder="Confirmar nueva contraseña"
+          placeholder="Apellidos"
+          marginY={[1, 1, 5]}
+          marginX={[4, 4, 5]}
+          minW="300px"
+          maxW="400px"
+          focusBorderColor="#4DDA73"
+          colorScheme={"#EBEEF1"}
+          bg="#EBEEF1"
+        />
+      </Box>
+
+      <Box>
+        <Text
+          fontWeight="bold"
+          fontSize={["x-large", "x-large", "xx-large"]}
+          borderBottom="1px solid #B5B5B5"
+        >
+          Información de contacto
+        </Text>
+      </Box>
+
+      <Box display={"flex"} paddingBottom="25px" flexWrap="wrap">
+        <Input
+          variant="filled"
+          placeholder="Email"
+          marginY={[3, 3, 5]}
+          marginX={[4, 4, 5]}
+          minW="300px"
+          maxW="400px"
+          focusBorderColor="#4DDA73"
+          colorScheme={"#EBEEF1"}
+          bg="#EBEEF1"
+        />
+        <Input
+          variant="filled"
+          placeholder="Télefono móvil"
           marginY={[1, 1, 5]}
           marginX={[4, 4, 5]}
           minW="300px"
@@ -51,15 +81,15 @@ function Contraseña() {
       <Box
         display={"flex"}
         flexWrap="wrap"
-        justifyContent={"center"}
+        justifyContent={["center", "center", "center", "flex-start"]}
         marginBottom={["20px"]}
       >
         <Button bg="#4DDA73" color="white" w={"120px"} borderRadius="15px">
           Guardar
         </Button>
       </Box>
-    </Box>
+    </>
   );
 }
 
-export default Contraseña;
+export default InputProfile;
--- web/src/components/Perfil/Password.tsx
+++ web/src/components/Perfil/Password.tsx
@@ -5,63 +5,23 @@ import candado from "../../assets/img/candado.svg";
 import { wrap } from "popmotion";
 import { Link } from "react-router-dom";
 
-function InputPerfil() {
+function Password() {
   return (
-    <Box
-      display={"flex"}
-      paddingX={[4, 4, 10]}
-      flexDirection="column"
-      w={["full"]}
-    >
+    <>
       <Box marginTop={["15px", "20px"]}>
         <Text
           fontWeight="bold"
           fontSize={["x-large", "x-large", "xx-large"]}
           borderBottom="1px solid #B5B5B5"
         >
-          Detalles del perfil
-        </Text>
-      </Box>
-
-      <Box display={"flex"} flexDirection={"row"} flexWrap="wrap">
-        <Input
-          variant="filled"
-          placeholder="Nombres"
-          marginY={[3, 3, 5]}
-          marginX={[4, 4, 5]}
-          minW="300px"
-          maxW="400px"
-          focusBorderColor="#4DDA73"
-          colorScheme={"#EBEEF1"}
-          bg="#EBEEF1"
-        />
-        <Input
-          variant="filled"
-          placeholder="Apellidos"
-          marginY={[1, 1, 5]}
-          marginX={[4, 4, 5]}
-          minW="300px"
-          maxW="400px"
-          focusBorderColor="#4DDA73"
-          colorScheme={"#EBEEF1"}
-          bg="#EBEEF1"
-        />
-      </Box>
-
-      <Box>
-        <Text
-          fontWeight="bold"
-          fontSize={["x-large", "x-large", "xx-large"]}
-          borderBottom="1px solid #B5B5B5"
-        >
-          Información de contacto
+          Cambiar contraseña
         </Text>
       </Box>
 
       <Box display={"flex"} paddingBottom="25px" flexWrap="wrap">
         <Input
           variant="filled"
-          placeholder="Email"
+          placeholder="Nueva contraseña"
           marginY={[3, 3, 5]}
           marginX={[4, 4, 5]}
           minW="300px"
@@ -72,7 +32,7 @@ function InputPerfil() {
         />
         <Input
           variant="filled"
-          placeholder="Télefono móvil"
+          placeholder="Confirmar nueva contraseña"
           marginY={[1, 1, 5]}
           marginX={[4, 4, 5]}
           minW="300px"
@@ -86,15 +46,15 @@ function InputPerfil() {
       <Box
         display={"flex"}
         flexWrap="wrap"
-        justifyContent={"center"}
+        justifyContent={["center", "center", "center", "flex-start"]}
         marginBottom={["20px"]}
       >
         <Button bg="#4DDA73" color="white" w={"120px"} borderRadius="15px">
           Guardar
         </Button>
       </Box>
-    </Box>
+    </>
   );
 }
 
-export default InputPerfil;
+export default Password;
