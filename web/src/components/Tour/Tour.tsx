8c0a14865881a1813483f533866b397007c7ddf0
Jhomar1158-ux <jhomarcristianelias@gmail.com>
2022-10-18 16:58:37 -0500

    add: chackraLink All Tours

--- web/src/components/Tour/Tour.tsx
+++ web/src/components/Tour/Tour.tsx
@@ -1,10 +1,46 @@
+import {
+  Box,
+  Button,
+  Image,
+  Input,
+  Text,
+  Link as ChakraLink,
+} from "@chakra-ui/react";
 
+import React, { useEffect, useState } from "react";
+import ArrowLeft from "../../icons/ArrowLeft";
+import { Link as RouterLink, Outlet, useLocation } from "react-router-dom";
 
 function Tour() {
-    return(
-        <>
-        </>
-    )
+  return (
+    <Box>
+      <Box flex={1} marginX={"40px"}>
+        <ChakraLink
+          as={RouterLink}
+          to="/tours"
+          w={["full", "full", "full"]}
+          flex={1}
+          h={["50px", "50px", "50px"]}
+          display={"flex"}
+          alignItems={"center"}
+        >
+          <Box marginX={"12px"}>
+            <ArrowLeft />
+          </Box>
+          <Text
+            display={"flex"}
+            fontWeight="bold"
+            fontSize="medium"
+            color="#4DDA73"
+          >
+            Ver todos los Tours
+          </Text>
+        </ChakraLink>
+        Senderismo por la montaña Arcoíris y el valle Rojo
+      </Box>
+      <Box>Mapa</Box>
+    </Box>
+  );
 }
 
 export default Tour;
