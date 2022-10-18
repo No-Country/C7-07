f0abfdc23ec8817e7a2fc0052b54799661c63b41
Matias Lupa <matiaslupa@gmail.com>
2022-10-18 14:57:58 -0300

    Itour merg

--- web/src/interfaces/ITour.ts
+++ web/src/interfaces/ITour.ts
@@ -1,14 +1,15 @@
 export interface ITour {
-  id: string;
-  title: string;
-  description: string;
-  region: string;
-  days: number;
-  country: string;
-  experience: Array<any>;
-  agency: Array<any | string>;
-  personPriceUsd: string;
-  city: string;
-  mainImages: Array<string>;
+    id: string;
+    title: string;
+    description: string;
+    region: string;
+    days: number;
+    country: string;
+    experience: Array<any>;
+    agency: Array<any | string>;
+    personPriceUsd: string;
+    city: string;
+    mainImages: Array<string>;
+    
+  }
   
-}
