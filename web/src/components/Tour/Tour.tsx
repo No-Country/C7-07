a4284cf9b8394964f2612a926cf894754e76ccb3
Jhomar1158-ux <jhomarcristianelias@gmail.com>
2022-10-20 00:55:56 -0500

    add: spinner and list

--- web/src/components/Tour/Tour.tsx
+++ web/src/components/Tour/Tour.tsx
@@ -5,6 +5,12 @@ import {
   Input,
   Text,
   Link as ChakraLink,
+  List,
+  ListItem,
+  ListIcon,
+  OrderedList,
+  UnorderedList,
+  Spinner,
 } from "@chakra-ui/react";
 
 import React, { useEffect, useState } from "react";
@@ -16,7 +22,7 @@ import { loadTourById, selectTour } from "../../features/tour/tourSlice";
 import { ITour } from "../../interfaces/ITour";
 import { useAppDispatch, useAppSelector } from "../../app/hooks";
 
-
+import checkIcon from "../../assets/img/check 3.svg";
 
 // Revisar primer param de getTourbyId
 // Correr url de Postman con agencia/tour
@@ -35,45 +41,89 @@ function Tour() {
     let {tourId} = useParams();
     const dispatch: any = useAppDispatch();
     const tourData = useAppSelector<ITour[] | null>(selectTour);
+    const [isLoading, setIsLoading] = useState(true);
 
     useEffect(() => {
         dispatch(loadTourById(tourId));
+        setTimeout(() => {
+            setIsLoading(false);
+        }, 5000);
     }, []);
+
     return (
         <Box  marginX={"40px"}>
-            <Box flex={1}>
-                {tourData && "SÏ"}
-                <ChakraLink
-                as={RouterLink}
-                to="/tours"
-                w={["full", "full", "full"]}
-                flex={1}
-                h={["50px", "50px", "50px"]}
-                display={"flex"}
-                alignItems={"center"}
-                >
-                <Box marginX={"10px"}>
-                    <ArrowLeft />
-                </Box>
-                <Text
-                    display={"flex"}
-                    fontWeight="bold"
-                    fontSize="medium"
-                    color="#4DDA73"
-                >
-                    Ver todos los Tours
-                </Text>
-                </ChakraLink>
-                <Box>
-                    <Box>
-                        {tourData.title}
-                    </Box>
-                    <Box>
-                        Puno, Perú - 1 día
-                    </Box>
-                </Box>
-            </Box>
-            <Box>Mapa</Box>
+            {isLoading ? <Spinner
+                            thickness='4px'
+                            speed='0.65s'
+                            emptyColor='gray.200'
+                            color='#4ED972'
+                            size='xl'
+                            display={"flex"}
+                            marginLeft={"630px"}
+                            marginTop={"250px"}
+                            />
+                        :
+                        <>
+                            <Box flex={1}>
+                                <ChakraLink
+                                as={RouterLink}
+                                to="/tours"
+                                w={["full", "full", "full"]}
+                                flex={1}
+                                h={["50px", "50px", "50px"]}
+                                display={"flex"}
+                                alignItems={"center"}
+                                >
+                                <Box marginX={"10px"}>
+                                    <ArrowLeft />
+                                </Box>
+                                <Text
+                                    display={"flex"}
+                                    fontWeight="bold"
+                                    fontSize="medium"
+                                    color="#4DDA73"
+                                >
+                                    Ver todos los Tours
+                                </Text>
+                                </ChakraLink>
+                                <Box>
+                                    <Box>
+                                        {tourData.title}
+                                    </Box>
+                                    <Box>
+                                    {tourData.city}, {tourData.country} · {tourData.days} dia{tourData.days > 1 ? "s": ""}
+                                    </Box>
+                                </Box>
+                            </Box>
+                            <Box>Mapa</Box>
+                            <Box>
+                                <Box w={["full", "full", "800px"]}>
+                                    {tourData.description}
+                                    <Box>
+                                        <Text>
+                                            Tu experiencia
+                                        </Text>
+                                        <Box>
+                                            <Text>
+                                                Qué harás
+                                            </Text>
+                                            <List>
+                                                <ListItem display="flex">
+                                                    <Image marginRight={"10px"} src={checkIcon}/>
+                                                    <Text>
+                                                    Lorem ipsum dolor sit amet, consectetur adipisicing elit
+                                                    </Text>
+                                                </ListItem>
+                                            </List>
+                                        </Box>
+                                    </Box>
+                                </Box>
+                                <Box>
+                                    Form
+                                </Box>
+                            </Box>
+                        </>
+            }
         </Box>
     );
 }
