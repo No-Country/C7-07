6bf9f9170925cd090a779d9dc048152efc2df319
IgnFed <ignaciofedorenco2317@gmail.com>
2022-10-21 11:50:10 -0300

    feat: new route <users/me>

--- server/src/controllers/users/index.ts
+++ server/src/controllers/users/index.ts
@@ -1,6 +1,7 @@
 import { getAllUsers } from "./getAllUsers";
 import { getUserById } from "./getUserById";
+import { getUserInfo } from "./getUserInfo";
 import { editUser } from "./editUser";
 import { removeUser } from "./removeUser";
 
-export { getAllUsers, getUserById, editUser, removeUser };
+export { getAllUsers, getUserById, getUserInfo, editUser, removeUser };
--- server/src/routes/user.routes.ts
+++ server/src/routes/user.routes.ts
@@ -4,12 +4,14 @@ import {
   editUser,
   getAllUsers,
   getUserById,
+  getUserInfo,
   removeUser,
 } from "../controllers/users";
 import { setPayload } from "../middlewares/setPayload";
 const router = Router();
 
 router.get("/", verifyToken, setPayload, getAllUsers);
+router.get("/me", verifyToken, setPayload, getUserInfo);
 router.get("/:userId", verifyToken, getUserById);
 router.put("/", verifyToken, setPayload, editUser);
 router.delete("/", verifyToken, setPayload, removeUser);
